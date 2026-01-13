3ece8b95c98d7cff8bb3cff001490877cf9c7ace
Aryan Pandey <ap408284@gmail.com>
2026-01-13 18:52:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const input = document.getElementById('numInput')
+const button = document.getElementById('btn')
+const output = document.getElementById('output')
+
+let numbers = []
+
+button.addEventListener("click", () => {
+    const value = Number(input.value);
+
+    numbers.push(value)
+
+    const evennumbers = numbers.filter( n => n % 2 === 0);
+     
+    output.innerText = evennumbers.join(", ")
+
+    input.value = ""
+})
